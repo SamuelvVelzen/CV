14ccf89d2f8b1391db69ece52bf1a5c95ac8fe4e
Samuel <samuel@elevatedigital.nl>
2021-03-03 19:32:00 +0100

    testing scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/style/index.scss';
+import './styles/style/utilities/theme.scss';
 import App from './pages/app';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <div className={`App light`}>
+            <App />
+        </div>
     </React.StrictMode>,
     document.getElementById('root'),
 );
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,5 +2,7 @@ import * as React from 'react';
 import { ReactElement } from 'react';
 
 export default function App(): ReactElement {
+    const test = { hoi: 'nee' };
+    console.log(test.hoi);
     return <p>testing</p>;
 }
