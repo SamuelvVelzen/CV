7692ed0c46431cf8765e4e1c7da0f9763bd67bf4
Samuel <samuel@elevatedigital.nl>
2021-03-06 17:19:18 +0100

    adding footer and edit error page for testing

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -5,6 +5,7 @@ import Introduction from './pages/introduction';
 import Error from './pages/error';
 import Navbar from './components/navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/footer';
 
 export default function App(): ReactElement {
     return (
@@ -14,10 +15,15 @@ export default function App(): ReactElement {
                     <div className={'app ' + theme}>
                         <Router>
                             <Navbar />
-                            <Switch>
-                                <Route exact path='/' component={Introduction} />
-                                <Route path='*' component={Error} />
-                            </Switch>
+
+                            <main>
+                                <Switch>
+                                    <Route exact path='/' component={Introduction} />
+                                    <Route path='*' component={Error} />
+                                </Switch>
+                            </main>
+
+                            <Footer />
                         </Router>
                     </div>
                 </>
--- src/pages/pages/error.tsx
+++ src/pages/pages/error.tsx
@@ -1,7 +1,38 @@
 import React, { ReactElement } from 'react';
 
 function Error(): ReactElement {
-    return <p>error</p>;
+    return (
+        <>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+            <p>error</p>
+        </>
+    );
 }
 
 export default Error;
